7e1ae05683a99c88abff42d6dee2a1e7fc1907a9
imamaulana26 <imamaulana26.github@gmail.com>
2019-07-01 22:01:58 +0700

    perubahan tampilan menu

--- application/views/templates/_navbar.php
+++ application/views/templates/_navbar.php
@@ -31,26 +31,30 @@
         <!-- Query Submenu -->
         <?php $role_id = $this->session->userdata('role_id');
         $this->db->select('*')->from('tbl_user_menu a')->join('tbl_user_role b', 'a.role_id = b.id', 'inner');
-        $this->db->join('tbl_menu c', 'c.id = a.menu_id', 'inner')->where(['a.role_id' => $role_id, 'a.active' => 1]);
+        $this->db->join('tbl_menu c', 'c.id = a.menu_id', 'inner')->where(['a.role_id' => $role_id, 'a.menu_id !=' => 1, 'a.active' => 1]);
         $this->db->order_by('a.menu_id', 'asc');
-        $menu = $this->db->get()->result_array(); ?>
-
+        $menu = $this->db->get()->result_array();
+        $dashboard = $this->db->get_where('tbl_user_menu', ['role_id' => $role_id, 'menu_id' => 1])->row_array(); ?>
         <div class="sidebar-nav navbar-collapse">
             <!-- Looping Submenu -->
-            <?php foreach ($menu as $m) { ?>
-                <ul class="nav" id="side-menu">
-                    <li><a href="<?= site_url(ucfirst($m['url'])) ?>"><i class="<?= $m['icon'] ?>"></i> <?= $m['menu'] ?></a></li>
-                </ul>
-            <?php } ?>
             <ul class="nav" id="side-menu">
+                <li><a href="<?= site_url(ucfirst($dashboard['url'])) ?>"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a></li>
                 <li>
-                    <a href="<?= site_url(ucfirst('order')) ?>">
-                        <i class="fa fa-fw fa-sitemap"></i> Request Order <span class="badge" style="background-color: red">42</span>
+                    <a href="#">
+                        <i class="fa fa-fw fa-sitemap"></i> Main Menu<span class="fa arrow"></span>
                     </a>
+                    <ul class="nav nav-second-level">
+                        <?php foreach($menu as $m){ ?>
+                            <li>
+                                <a href="<?= site_url(ucfirst($m['url'])) ?>"><i class="<?= $m['icon'] ?>"></i> <?= $m['menu'] ?></a>
+                            </li>
+                        <?php } ?>
+                    </ul>
+                    <!-- /.nav-second-level -->
                 </li>
-            </ul>
-            <ul class="nav" id="side-menu">
-                <li><a href="<?= site_url(ucfirst('auth/logout')) ?>"><i class="fa fa-fw fa-sign-out"></i> Logout</a></li>
+                <li>
+                    <a href="<?= site_url(ucfirst('auth/logout')) ?>"><i class="fa fa-fw fa-sign-out"></i> Logout</a>
+                </li>
             </ul>
         </div>
         <!-- /.sidebar-collapse -->
